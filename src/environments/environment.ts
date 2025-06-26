4228ed7ef72d651a24c585df1075217495aa629b
bhowmik94 <sourav.bhowmik@stud.uni-bamberg,de>
2025-06-26 13:55:05 +0200

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCkwjb590N58_DcVBUS2CPHAU6COy0-q_A',
+    authDomain: 'decision-tracker-fe603.firebaseapp.com',
+    projectId: 'decision-tracker-fe603',
+    storageBucket: 'decision-tracker-fe603.firebasestorage.app',
+    messagingSenderId: '580155633223',
+    appId: '1:580155633223:web:788c3bf9b0f99238cb4b62',
+    measurementId: 'G-R049Q7JQGV',
+  },
+};
